1247dfc1f62899b30b24a59a2654bdc4df9b8785
chacuavip10 <chacuavip10@gmail.com>
2020-10-24 20:26:14 +0700

    new_script

--- script_acc_kh.py
+++ script_acc_kh.py
@@ -20,14 +20,8 @@
 # Jnpr-IPv6-Delegated-Pool-Name: FTTH-V6-LAN
 # Framed-Route: 1.1.1.1/32
 # Framed-Route: 2.2.2.2/32
-import prettytable
-from prettytable import from_csv
+import pyperclip
 import ipaddress
-# Add goi cuoc
-fp = open("goicuoc.csv", "r")
-goicuoc = from_csv(fp)
-fp.close()
-goicuoc.align = "l"
 list_goi_cuoc = {1: "CN1", 2: "CN2", 3: "CN3", 4: "CN4", 5: "CN5",
                  6: "CN6", 7: "VTVnet-S30", 8: "VTVnet-S40", 9: "VTVnet-S50",
                  10: "VTVnet-S60", 11: "VTVnet-S70", 12: "VTVnet-BUSINESS-80",
@@ -38,8 +32,13 @@ ldif = ""
 # Init
 add_user = "y"
 while add_user == "y":
+    # Variable for Customer type
     cgnat = False
     dynamic_sub = True
+    static_ip = False
+    One_block_ip = False  # neu k/h co 2 block ip, bien nay se de gia tri la True
+    Two_block_ip = False
+    # Variable for LDIF
     dn_start = "dn: uid="
     dn_end = ",ou=hni,dc=mobifone,dc=vn"
     common = """\nchangetype: add
@@ -52,13 +51,15 @@ objectClass: ftth-postpaid"""
     sn = "\nsn: "
     uid = "\nuid: "
     cn = "\ncn: "
-    userPassword = "\n1a2b3c4d"  # Default Password
+    userPassword_default = "\nuserPassword: 1a2b3c4d"  # Default Password
+    userPassword = "\nuserPassword: "  # Default Password
     ipv4_upload = "\nUnisphere-Ingress-Policy-Name: U-"
     ipv4_download = "\nUnisphere-Egress-Policy-Name: D-"
     ipv6_upload = "\nJnpr-IPv6-Ingress-Policy-Name: ipv6-U-"
     ipv6_download = "\nJnpr-IPv6-Egress-Policy-Name: ipv6-D-"
     ipv4_static = "\nFramed-Ip-Address: "
     ipv4_netmask = "\nFramed-Ip-Netmask: 255.255.255.255"
+    ipv4_pool = ""  # ipv4_pool = "\nFramed-Pool: BRAS_01" # Them ipv4 pool neu ko su dung Pool Default
     ipv4_pool_cgnat = "\nFramed-Pool: ftth_private"
     ipv6_pool = """\nFramed-IPv6-Pool: FTTH-V6-WAN
 Jnpr-IPv6-Delegated-Pool-Name: FTTH-V6-LAN"""
@@ -70,12 +71,15 @@ Jnpr-IPv6-Delegated-Pool-Name: FTTH-V6-LAN-CGNAT"""
     username = input("(Input) Username/UID: ")
     pwd = input("(Input) Password(1 to user default 1a2b3c4d): ")
     if pwd == "1":
+        userPassword = userPassword_default
         print("==>(Info) Password default to: 1a2b3c4d")
     else:
-        userPassword = "\n" + pwd
+        userPassword += pwd
     print("===================================================")
     # goi cuoc
-    print(goicuoc.get_string())
+    for x in list_goi_cuoc:
+        print(f"{x}: {list_goi_cuoc[x]}")
+    print("===================================================")
     id = 100
     while (int(id) >= 21) or (int(id) <= 0):  # change condition neu them goi cuoc
         plan_id = input("(Input) ID goi cuoc? (1-20): ")
@@ -83,9 +87,41 @@ Jnpr-IPv6-Delegated-Pool-Name: FTTH-V6-LAN-CGNAT"""
     # Lay ten goi cuoc dua theo ID tu list_goi_cuoc
     plan = list_goi_cuoc.get(int(plan_id))
     print(f"==>(Info) Goi cuoc: {plan}")
-    #############
+    #######################################################################################################
+    #### *************Neu them goi cuoc, thay doi doan code nay cho phu hop***************************####
+    if 1 <= int(plan_id) <= 6:
+        cgnat = True
+        dynamic_sub = True
+        static_ip = False
+        One_block_ip = False
+        Two_block_ip = False
+    elif 7 <= int(plan_id) <= 11:
+        cgnat = False
+        dynamic_sub = True
+        static_ip = False
+        One_block_ip = False
+        Two_block_ip = False
+    elif 12 <= int(plan_id) <= 17:
+        cgnat = False
+        dynamic_sub = False
+        static_ip = True
+        One_block_ip = False
+        Two_block_ip = False
+    elif 18 <= int(plan_id) <= 19:
+        cgnat = False
+        dynamic_sub = False
+        static_ip = True
+        One_block_ip = True
+        Two_block_ip = False
+    elif int(plan_id) == 20:
+        cgnat = False
+        dynamic_sub = False
+        static_ip = True
+        One_block_ip = True
+        Two_block_ip = True
+    #############################################################
     # Input k/h ip tinh (public ip)
-    if 12 <= int(plan_id) <= 20:  # change condition neu them goi cuoc
+    if static_ip:  # equal static_ip == True
         print("==>(Info) Goi cuoc bao gom 01 ip tinh!")
         loop = True
         while loop == True:
@@ -101,18 +137,18 @@ Jnpr-IPv6-Delegated-Pool-Name: FTTH-V6-LAN-CGNAT"""
                 print("==> (Error) Sai cu phap dia chi ip! (x.x.x.x, x <= 255)")
         sub_ipv4_static_input = ipv4_static + \
             IP_Static + ipv4_netmask  # Update ip static
-    # Input block ip
-    if int(plan_id) == 20:  # change condition neu them goi cuoc
+    # Info
+    if One_block_ip & Two_block_ip:  # One_block_ip = Two_block_ip = True
         print("==>(Info) Goi cuoc bao gom 02 block ip tinh!")
-    elif 18 <= int(plan_id) <= 19:
+    elif (One_block_ip == True) & (Two_block_ip == False):
         print("==>(Info) Goi cuoc bao gom 01 block ip tinh!")
-
-    if 18 <= int(plan_id) <= 20:  # change condition neu them goi cuoc
+    # Input block ip 1
+    if One_block_ip | Two_block_ip:  # One_block_ip = True or Two_block_ip = True
         loop = True
         while loop == True:
             try:
                 Framed_Route_1 = ipaddress.ip_network(
-                    input("(Input) Block IP, subnetmask default /32: "))
+                    input("(Input) Block IP , subnetmask default /32: "))
                 if ipaddress.ip_network(Framed_Route_1).is_private == True:
                     loop = True
                     print(
@@ -123,8 +159,8 @@ Jnpr-IPv6-Delegated-Pool-Name: FTTH-V6-LAN-CGNAT"""
                 print("==> (Error) Sai cu phap block ip! (ipaddress/subnetmask)")
         sub_ipv4_static_input = sub_ipv4_static_input + \
             "\nFramed-Route: " + str(Framed_Route_1)
-
-    if int(plan_id) == 20:  # change condition neu them goi cuoc
+    # Input block ip 2
+    if Two_block_ip:  # change condition neu them goi cuoc
         loop = True
         while loop == True:
             try:
@@ -140,18 +176,13 @@ Jnpr-IPv6-Delegated-Pool-Name: FTTH-V6-LAN-CGNAT"""
                 print("==> (Error) Sai cu phap block ip! (ipaddress/subnetmask)")
         sub_ipv4_static_input = sub_ipv4_static_input + \
             "\nFramed-Route: " + str(Framed_Route_2)
-
-    if 18 <= int(plan_id) <= 19:
+    # Print block ip
+    if (One_block_ip == True) & (Two_block_ip == False):
         print(f"==>(Info) Block IP: {Framed_Route_1}")
-    elif int(plan_id) == 20:
+    elif Two_block_ip:
         print(f"==>(Info) Block IP: {Framed_Route_1} {Framed_Route_2}")
     # Generate LDIF file
     # 0. Update variable
-    # check CGNAT?
-    if 1 <= int(plan_id) <= 6:  # change condition neu them goi cuoc
-        cgnat = True  # $dynamic_sub = True
-    if int(plan_id) >= 12:
-        dynamic_sub = False
     givenname += username
     sn += username
     uid += username
@@ -162,14 +193,14 @@ Jnpr-IPv6-Delegated-Pool-Name: FTTH-V6-LAN-CGNAT"""
     ipv6_upload += plan
     ipv6_download += plan
     # 1. add dn, uid
-    if 1 <= int(plan_id) <= 6:  # update neu update list goi cuoc
+    if cgnat:  # update neu update list goi cuoc
         ldif = ldif + dn_start + username + ",cn=" + plan + dn_end
     else:
         ldif = ldif + dn_start + username + dn_end
     # 2. add common
     ldif = ldif + common
     # 3. add account info
-    ldif = ldif + givenname + sn + uid + cn
+    ldif = ldif + givenname + sn + uid + cn + userPassword
     # 4. add policy upload/download ipv4, ipv6
     ldif = ldif + ipv4_download + ipv4_upload + ipv6_download + ipv6_upload
     # 5. cho k/h CGNAT ==> End of Ldif
@@ -177,22 +208,37 @@ Jnpr-IPv6-Delegated-Pool-Name: FTTH-V6-LAN-CGNAT"""
     if (cgnat == True) & (dynamic_sub == True):
         ldif = ldif + ipv4_pool_cgnat + ipv6_pool_cgnat + vrf_cgnat
     if (cgnat == False) & (dynamic_sub == True):
-        ldif = ldif + ipv6_pool
+        ldif = ldif + ipv4_pool + ipv6_pool
     # 6. cho k/h ip tinh, no block ip
     ldif = ldif + sub_ipv4_static_input
     ldif += "\n\n"
-    print(f"==>(Info) Add user {username} to ldif file!")
+    print(f"==>(Info) Add user <{username}> to ldif file!")
     add_user = input(
         "\n==>(Input) Them khach hang? (choose y for yes, any other keys for no): ")
 ##Debug###
 # print(sub_ipv4_static_input)
+
+##########################LDIF OUTPUT#########################################################
 ldif = ldif.rstrip()
 ldif = ldif.lstrip()
-print("============================================")
+print("================================================================================\n")
+print(""" .----------------.  .----------------.  .----------------.  .----------------. 
+| .--------------. || .--------------. || .--------------. || .--------------. |
+| |   _____      | || |  ________    | || |     _____    | || |  _________   | |
+| |  |_   _|     | || | |_   ___ `.  | || |    |_   _|   | || | |_   ___  |  | |
+| |    | |       | || |   | |   `. \ | || |      | |     | || |   | |_  \_|  | |
+| |    | |   _   | || |   | |    | | | || |      | |     | || |   |  _|      | |
+| |   _| |__/ |  | || |  _| |___.' / | || |     _| |_    | || |  _| |_       | |
+| |  |________|  | || | |________.'  | || |    |_____|   | || | |_____|      | |
+| |              | || |              | || |              | || |              | |
+| '--------------' || '--------------' || '--------------' || '--------------' |
+ '----------------'  '----------------'  '----------------'  '----------------' """)
+print("================================================================================\n")
 print(ldif)
-print("============================================")
-print("Write to add_new_user.ldif!")
+print("\n============================================")
 # Write to file
+pyperclip.copy(ldif)
 f = open("add_new_user.ldif", "w")
 f.write(ldif)
 f.close()
+input("Write to Clipboard & add_new_user.ldif!, press any key to quit!")
